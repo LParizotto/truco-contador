51ecd81315e4d0314a8629f3e99bb7adf6988641
Luís Fernando Fappi Parizotto <parizotto011@gmail.com>
2026-03-16 20:16:06 -0300

    feat: adiciona placar para dois times

--- App.js
+++ App.js
@@ -5,23 +5,47 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 export default function App() {
 
   const [contador, setContador] = useState(0);
+  const [contador2, setContador2] = useState(0);
+
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>MARCADOR</Text>
-      
-      <Text style={styles.count}>{contador}</Text>
 
-    <View style={styles.buttons}>
+      <View style={styles.section}>
+        <Text style={styles.title}>NÓS</Text>
 
-    <View style={styles.button}>
-      <Button 
-      color={'#157512'} title='+' onPress={() => setContador(contador + 1)} />
+        <Text style={styles.count}>{contador}</Text>
+
+        <View style={styles.buttons}>
+
+          <View style={styles.button}>
+            <Button
+              color={'#157512'} title='+' onPress={() => setContador(contador + 1)} />
           </View>
-    <View style={styles.button}>
-      <Button color={'#ff0000'} title='-' onPress={() => contador > 0 && setContador(contador - 1)} />
-    </View>
-    </View>
+          <View style={styles.button}>
+            <Button color={'#8d0000'} title='-' onPress={() => contador > 0 && setContador(contador - 1)} />
+          </View>
+        </View>
+      </View>
+
+
+      <View style={styles.section}>
+        <Text style={styles.title}>ELES</Text>
+
+        <Text style={styles.count}>{contador2}</Text>
+
+        <View style={styles.buttons}>
+
+          <View style={styles.button}>
+            <Button
+              color={'#157512'} title='+' onPress={() => setContador2(contador2 + 1)} />
+          </View>
+          <View style={styles.button}>
+            <Button color={'#8d0000'} title='-' onPress={() => contador2 > 0 && setContador2(contador2 - 1)} />
+          </View>
+        </View>
+      </View>
+
 
       <StatusBar style="auto" />
     </View>
@@ -30,15 +54,20 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex1: 1,
+    flexDirection: 'row',
+    marginTop: 200,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold', 
+    fontWeight: 'bold',
     marginBottom: 20,
+  },
+  section: {
+    width: '50%',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   count: {
     fontSize: 60,
