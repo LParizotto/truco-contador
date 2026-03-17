65c584bc569df28e7a6cbf889ccc17d41316f4e7
Luís Fernando Fappi Parizotto <parizotto011@gmail.com>
2026-03-17 10:25:06 -0300

    feat: adiciona controle de reinício da partida

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import { Image } from "react-native";
 
 export default function App() {
   const [contador, setContador] = useState(0);
@@ -10,123 +11,148 @@ export default function App() {
   const [win2, setWin2] = useState(0);
 
   function contadorNos(valor) {
-  const novo = contador + valor;
+    const novo = contador + valor;
 
-  if (novo >= 12) {
+    if (novo >= 12) {
+      setContador(0);
+      setContador2(0);
+      setWin((prev) => prev + 1);
+    } else {
+      setContador(novo);
+    }
+  }
+
+  function contadorEles(valor) {
+    const novo = contador2 + valor;
+
+    if (novo >= 12) {
+      setContador(0);
+      setContador2(0);
+      setWin2((prev) => prev + 1);
+    } else {
+      setContador2(novo);
+    }
+  }
+
+  function restart() {
     setContador(0);
     setContador2(0);
-    setWin((prev) => prev + 1);
-  } else {
-    setContador(novo);
   }
-}
 
-function contadorEles(valor) {
-  const novo = contador2 + valor;
-
-  if (novo >= 12) {
+  function newGame() {
     setContador(0);
     setContador2(0);
-    setWin2((prev) => prev + 1);
-  } else {
-    setContador2(novo);
+    setWin(0);
+    setWin2(0);
   }
-}
 
   return (
     <View style={styles.container}>
-      <View style={styles.section}>
-        <Text style={styles.title}>NÓS</Text>
+      <Image
+        source={{
+          uri: "https://toppng.com/uploads/preview/logo-unipar5-horizontal-sem-fundo-logotipo-unipar-11563207391ynw4uhpenk.png",
+        }}
+        style={{ width: 150, height: 100 }}
+      />
+      <View style={styles.counters}>
+        <View style={styles.section}>
+          <Text style={styles.title}>NÓS</Text>
 
-        <Text style={styles.count}>{contador}</Text>
+          <Text style={styles.count}>{contador}</Text>
 
-        <Text>Ganhou: {win} </Text>
+          <Text>Ganhou: {win}</Text>
 
-        <View style={styles.buttons}>
-          <View style={styles.button}>
-            <Button
-              color={"#157512"}
-              title="+"
-              onPress={() => contadorNos(1)}
-            />
+          <View style={styles.buttons}>
+            <View style={styles.button}>
+              <Button
+                color={"#157512"}
+                title="+"
+                onPress={() => contadorNos(1)}
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                color={"#8d0000"}
+                title="-"
+                onPress={() => contador > 0 && setContador(contador - 1)}
+              />
+            </View>
           </View>
-          <View style={styles.button}>
+          <View style={styles.buttonsespeciais}>
             <Button
-              color={"#8d0000"}
-              title="-"
-              onPress={() => contador > 0 && setContador(contador - 1)}
+              color={"#0067ac"}
+              title="Truco"
+              onPress={() => contadorNos(3)}
+            />
+            <Button
+              color={"#000981"}
+              title="Seis"
+              onPress={() => contadorNos(6)}
+            />
+            <Button
+              color={"#5f009e"}
+              title="Nove"
+              onPress={() => contadorNos(9)}
+            />
+            <Button
+              color={"#580000"}
+              title="Doze"
+              onPress={() => contadorNos(12)}
             />
           </View>
         </View>
-        <View style={styles.buttonsespeciais}>
-          <Button
-            color={"#0067ac"}
-            title="Truco"
-            onPress={() => contadorNos(3)}
-          />
-          <Button
-            color={"#000981"}
-            title="Seis"
-            onPress={() => contadorNos(6)}
-          />
-          <Button
-            color={"#5f009e"}
-            title="Nove"
-            onPress={() => contadorNos(9)}
-          />
-          <Button
-            color={"#580000"}
-            title="Doze"
-            onPress={() => contadorNos(12)}
-          />
+
+        <View style={styles.section}>
+          <Text style={styles.title}>ELES</Text>
+
+          <Text style={styles.count}>{contador2}</Text>
+
+          <Text>Ganhou: {win2}</Text>
+
+          <View style={styles.buttons}>
+            <View style={styles.button}>
+              <Button
+                color={"#157512"}
+                title="+"
+                onPress={() => contadorEles(1)}
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                color={"#8d0000"}
+                title="-"
+                onPress={() => contador2 > 0 && setContador2(contador2 - 1)}
+              />
+            </View>
+          </View>
+          <View style={styles.buttonsespeciais}>
+            <Button
+              color={"#0067ac"}
+              title="Truco"
+              onPress={() => contadorEles(3)}
+            />
+            <Button
+              color={"#000981"}
+              title="Seis"
+              onPress={() => contadorEles(6)}
+            />
+            <Button
+              color={"#5f009e"}
+              title="Nove"
+              onPress={() => contadorEles(9)}
+            />
+            <Button
+              color={"#580000"}
+              title="Doze"
+              onPress={() => contadorEles(12)}
+            />
+          </View>
         </View>
       </View>
 
-      <View style={styles.section}>
-        <Text style={styles.title}>ELES</Text>
-
-        <Text style={styles.count}>{contador2}</Text>
-
-        <Text>Ganhou: {win2} </Text>
-
-        <View style={styles.buttons}>
-          <View style={styles.button}>
-            <Button
-              color={"#157512"}
-              title="+"
-              onPress={() => contadorEles(1)}
-            />
-          </View>
-          <View style={styles.button}>
-            <Button
-              color={"#8d0000"}
-              title="-"
-              onPress={() => contador2 > 0 && setContador2(contador2 - 1)}
-            />
-          </View>
-        </View>
-        <View style={styles.buttonsespeciais}>
-          <Button
-            color={"#0067ac"}
-            title="Truco"
-            onPress={() => contadorEles(3)}
-          />
-          <Button
-            color={"#000981"}
-            title="Seis"
-            onPress={() => contadorEles(6)}
-          />
-          <Button
-            color={"#5f009e"}
-            title="Nove"
-            onPress={() => contadorEles(9)}
-          />
-          <Button
-            color={"#580000"}
-            title="Doze"
-            onPress={() => contadorEles(12)}
-          />
-        </View>
+      <View style={styles.options}>
+        <Button color="#1a0000" title="REINICIAR" onPress={restart} />
+        <Button color="#1a0000" title="NOVO JOGO" onPress={newGame} />
       </View>
 
       <StatusBar style="auto" />
@@ -137,19 +163,22 @@ function contadorEles(valor) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
-    marginTop: 200,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 150,
     backgroundColor: "#fff",
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
-    marginBottom: 20,
+    marginBottom: 10,
   },
   section: {
     width: "50%",
     alignItems: "center",
     justifyContent: "center",
+    marginRight: 20,
   },
   count: {
     fontSize: 60,
@@ -158,7 +187,7 @@ const styles = StyleSheet.create({
   buttons: {
     flexDirection: "row",
     gap: 10,
-    marginTop: 100,
+    marginTop: 20,
   },
   button: {
     width: 50,
@@ -168,5 +197,16 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     gap: 10,
     width: 150,
+    fontSize: 40,
+  },
+  counters: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  options: {
+    width: 200,
+    height: 150,
+    gap: 10,
+    marginBottom: 100,
   },
 });
