5bfca8bd422542bdfea9c971f5fc3be96544310b
Luís Fernando Fappi Parizotto <parizotto011@gmail.com>
2026-03-17 09:40:14 -0300

    feat: adiciona contagem de vitórias para cada time

--- App.js
+++ App.js
@@ -1,63 +1,133 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-
   const [contador, setContador] = useState(0);
   const [contador2, setContador2] = useState(0);
 
+  const [win, setWin] = useState(0);
+  const [win2, setWin2] = useState(0);
+
+  function contadorNos(valor) {
+  const novo = contador + valor;
+
+  if (novo >= 12) {
+    setContador(0);
+    setContador2(0);
+    setWin((prev) => prev + 1);
+  } else {
+    setContador(novo);
+  }
+}
+
+function contadorEles(valor) {
+  const novo = contador2 + valor;
+
+  if (novo >= 12) {
+    setContador(0);
+    setContador2(0);
+    setWin2((prev) => prev + 1);
+  } else {
+    setContador2(novo);
+  }
+}
+
   return (
     <View style={styles.container}>
-
-
       <View style={styles.section}>
         <Text style={styles.title}>NÓS</Text>
 
         <Text style={styles.count}>{contador}</Text>
 
-        <View style={styles.buttons}>
+        <Text>Ganhou: {win} </Text>
 
+        <View style={styles.buttons}>
           <View style={styles.button}>
             <Button
-              color={'#157512'} title='+' onPress={() => setContador(contador + 1)} />
+              color={"#157512"}
+              title="+"
+              onPress={() => contadorNos(1)}
+            />
           </View>
           <View style={styles.button}>
-            <Button color={'#8d0000'} title='-' onPress={() => contador > 0 && setContador(contador - 1)} />
+            <Button
+              color={"#8d0000"}
+              title="-"
+              onPress={() => contador > 0 && setContador(contador - 1)}
+            />
           </View>
         </View>
         <View style={styles.buttonsespeciais}>
-          <Button color={'#0067ac'} title='Truco' onPress={() => setContador(contador + 3)} />
-          <Button color={'#000981'} title='Seis' onPress={() => setContador(contador + 6)} />
-          <Button color={'#5f009e'} title='Nove' onPress={() => setContador(contador + 9)} />
-          <Button color={'#580000'} title='Doze' onPress={() => setContador(contador + 12)} />
+          <Button
+            color={"#0067ac"}
+            title="Truco"
+            onPress={() => contadorNos(3)}
+          />
+          <Button
+            color={"#000981"}
+            title="Seis"
+            onPress={() => contadorNos(6)}
+          />
+          <Button
+            color={"#5f009e"}
+            title="Nove"
+            onPress={() => contadorNos(9)}
+          />
+          <Button
+            color={"#580000"}
+            title="Doze"
+            onPress={() => contadorNos(12)}
+          />
         </View>
       </View>
-
 
       <View style={styles.section}>
         <Text style={styles.title}>ELES</Text>
 
         <Text style={styles.count}>{contador2}</Text>
 
-        <View style={styles.buttons}>
+        <Text>Ganhou: {win2} </Text>
 
+        <View style={styles.buttons}>
           <View style={styles.button}>
             <Button
-              color={'#157512'} title='+' onPress={() => setContador2(contador2 + 1)} />
+              color={"#157512"}
+              title="+"
+              onPress={() => contadorEles(1)}
+            />
           </View>
           <View style={styles.button}>
-            <Button color={'#8d0000'} title='-' onPress={() => contador2 > 0 && setContador2(contador2 - 1)} />
+            <Button
+              color={"#8d0000"}
+              title="-"
+              onPress={() => contador2 > 0 && setContador2(contador2 - 1)}
+            />
           </View>
         </View>
         <View style={styles.buttonsespeciais}>
-          <Button color={'#0067ac'} title='Truco' onPress={() => setContador2(contador2 + 3)} />
-          <Button color={'#000981'} title='Seis' onPress={() => setContador2(contador2 + 6)} />
-          <Button color={'#5f009e'} title='Nove' onPress={() => setContador2(contador2 + 9)} />
-          <Button color={'#580000'} title='Doze' onPress={() => setContador2(contador2 + 12)} />
+          <Button
+            color={"#0067ac"}
+            title="Truco"
+            onPress={() => contadorEles(3)}
+          />
+          <Button
+            color={"#000981"}
+            title="Seis"
+            onPress={() => contadorEles(6)}
+          />
+          <Button
+            color={"#5f009e"}
+            title="Nove"
+            onPress={() => contadorEles(9)}
+          />
+          <Button
+            color={"#580000"}
+            title="Doze"
+            onPress={() => contadorEles(12)}
+          />
         </View>
       </View>
-
 
       <StatusBar style="auto" />
     </View>
@@ -66,27 +136,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex1: 1,
-    flexDirection: 'row',
+    flex: 1,
+    flexDirection: "row",
     marginTop: 200,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
   },
   section: {
-    width: '50%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "50%",
+    alignItems: "center",
+    justifyContent: "center",
   },
   count: {
     fontSize: 60,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   buttons: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 10,
     marginTop: 100,
   },
@@ -95,9 +165,8 @@ const styles = StyleSheet.create({
     height: 50,
   },
   buttonsespeciais: {
-    flexDirection: 'collumn',
+    flexDirection: "column",
     gap: 10,
     width: 150,
   },
-
 });
